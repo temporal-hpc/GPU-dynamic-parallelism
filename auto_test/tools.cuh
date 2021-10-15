6bd6d58a37332d66ca7fa282a0dc222818968d50
cristobal <cristobal.navarro.g@gmail.com>
2021-10-15 09:04:52 -0300

    improved info message for arguments

--- auto_test/tools.cuh
+++ auto_test/tools.cuh
@@ -4,21 +4,24 @@ using namespace std;
 
 void check_args_info(int argc){
     if (argc != 14) {
-        cout << "Error. Execute as ./mandelbrot <Approach> <W> <H> <rmin> <rmax> <cmin> <cmax> <CA_MAXDWELL> <B> <G0> <R> <MAX_DEPTH> <filename>" << endl;
-        cout << "Apporach:  0 - Brute Force" << endl;
-        cout << "           1 - Dynamic Parallelism" << endl;
-        cout << "           2 - Adaptive Serial Kernels" << endl;
-        cout << "Default values:" << endl;
-        cout << "   W ------------------------- Width in pixels." << endl;
-        cout << "   H ------------------------- Height in pixels." << endl;
-        cout << "   xlim -------- [-1.5, 0.5] - x range in the plane (real values)." << endl;
-        cout << "   ylim -------- [-1.0, 1.0] - y range in the plane (imaginary values)." << endl;
-        cout << "   CA_MAXDWELL --------------- 512 - Maximum numbers of iterarions per element." << endl;
-        cout << "   B ------------------------- 32 - Size of the regions when exhaustive is triggered (powers of 2)." << endl;
-        cout << "   G0 ------------------------ 32 - Initial numbers of regions (powers of 2)." << endl;
-        cout << "   r ------------------------- 4 - Subdivision scheme (powers of 2)." << endl;
-        cout << "   MAX_DEPTH ----------------- 5 - Maximum recursion depth." << endl;
-        cout << "   filename  ----------------- 'none' - Use 'none' to skip file output." << endl;
+        cout << "\nExecute as ./mandelbrot <Approach> <W> <H> <rmin> <rmax> <cmin> <cmax> <CA_MAXDWELL> <B> <g> <r> <MAX_DEPTH> <filename>" << endl;
+        cout << "   Approach:" << endl;
+        cout << "               0 - Exhaustive (classic one-pass approach)" << endl;
+        cout << "               1 - Dynamic Parallelism (from Nvidia)" << endl;
+        cout << "               2 - Adaptive Serial Kernels (alternative approach)" << endl;
+        cout << "   -----------------------------------------------------------------------------------------------" << endl;
+        cout << "   Parameters                  Default         Info" << endl;
+        cout << "   -----------------------------------------------------------------------------------------------" << endl;
+        cout << "   W ------------------------- 1024            Width in pixels." << endl;
+        cout << "   H ------------------------- 1024            Height in pixels." << endl;
+        cout << "   rmin rmax ----------------- [-1.5, 0.5]     Real part range." << endl;
+        cout << "   cmin cmax ----------------- [-1.0, 1.0]     Complex part range." << endl;
+        cout << "   CA_MAXDWELL --------------- 512             Maximum numbers of iterarions per element." << endl;
+        cout << "   B ------------------------- 32              Region Size for stopping subdivision (powers of 2)." << endl;
+        cout << "   g ------------------------- 32              Initial numbers of regions (powers of 2)." << endl;
+        cout << "   r ------------------------- 4               Subdivision scheme (powers of 2)." << endl;
+        cout << "   MAX_DEPTH ----------------- 5               Maximum recursion depth." << endl;
+        cout << "   filename  ----------------- none            Chosen filename, 'none' to skip file output." << endl;
         exit(-1);
     }
 }
