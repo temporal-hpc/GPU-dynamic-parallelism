8ce861c408e39f812b15cef15f6316cab9b30763
Cristobal Navarro <cristobal.navarro.g@gmail.com>
2021-10-18 23:51:44 -0300

    fixed output bug

--- src/main.cu
+++ src/main.cu
@@ -166,7 +166,7 @@ int main(int argc, char **argv) {
         printf("\n");
     #endif
 
-    printf("%i,%s   %i, %i,   %i, %i,   %i, %i,   %i, %i, %i,   %f, %f, %f, %f\n", approach, approachStr[approach], BSX, BSY, W, H, CA_MAXDWELL, MAX_DEPTH, g0, r, B, 
+    printf("%i,%s,   %i, %i,   %i, %i,   %i, %i,   %i, %i, %i,   %f, %f, %f, %f\n", approach, approachStr[approach], BSX, BSY, W, H, CA_MAXDWELL, MAX_DEPTH, g0, r, B, 
             stat.mean, stat.stdev, stat.sterr, 100.0*stat.sterr/stat.mean);
     exit(EXIT_SUCCESS);
 }
