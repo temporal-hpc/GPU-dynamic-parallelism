8f82d1d10dcaae5d41273f739f9a89a2b952c988
Cristobal Navarro <cristobal.navarro.g@gmail.com>
2021-11-11 10:19:44 -0300

    new order of params --> g,r,B

--- benchmark.sh
+++ benchmark.sh
@@ -20,7 +20,7 @@ OUTPUT=data/${STRING}-ARCH${ARCH}-REA${REAL}-REP${REPE}-BS${BS}.dat
 # COMPILE
 make -B ARCH=${ARCH} REALIZATIONS=${REAL}  REPEATS=${REPE} BSX=${BS} BSY=${BS} BENCHMARK=BENCHMARK
 echo "#NEW BENCHMARK ${STRING} ${ARCH}  ${DATE}        MAXDWELL=${CA_MAXDWELL}  MAX_DEPTH=${MAX_DEPTH}">> ${OUTPUT}
-echo "#N, g0,r,B,                   perfAP0                             perfA1                            perfA2                               perfA3" >> ${OUTPUT}
+echo "#N, g,r,B,                   perfAP0                             perfA1                            perfA2                               perfA3" >> ${OUTPUT}
 
 maxEXP=10
 NexpMAX=16
@@ -29,34 +29,28 @@ NexpMAX=16
 for ((size=0; size <= ${NexpMAX}; size++));
 do
     N=$((2**${size}))
-    #for g0exp in 2 4 8 16 32 64 128 256 512 1024
     lim=$((${size}<${maxEXP} ? ${size} : ${maxEXP}))
-    for ((g0exp=1; g0exp <= ${lim}; g0exp++));
+    for ((gexp=1; gexp <= ${lim}; gexp++));
     do
-        g0=$((2**${g0exp}))
-        aux=$((${size}-${g0exp}))
+        g=$((2**${gexp}))
+        aux=$((${size}-${gexp}))
         divlimit=$((${aux}<${maxEXP} ? ${aux} : ${maxEXP}))
-        #for r in 2 4 8 16 32 64 128 256 512 1024
-        #for rexp in {1..${divlimit}..1}
         for ((rexp=1; rexp <= ${divlimit}; rexp++));
         do
             r=$((2**${rexp}))
-            #for B in 1 2 4 8 16 32 64 128 256 512 1024
-            #for Bexp in {0..${divlimit}..1}
             for ((Bexp=0; Bexp <= ${divlimit}; Bexp++));
             do
                 B=$((2**${Bexp}))
-                a="${N},    ${g0},${r},${B}"
-                echo "BENCHMARK g0=${g0},r=${r},B=${B},    N=${N}"
+                a="${N},    ${g},${r},${B}"
+                echo "BENCHMARK g=${g},r=${r},B=${B},    N=${N}"
                 for approach in 0 1 2 3
                 do
                     echo -n -e "\tA${approach}......"
-                    res=$(exec ${GPUPROG} $approach ${N} ${N} -1.5 0.5 -1 1 ${CA_MAXDWELL} ${B} ${g0} ${r} $MAX_DEPTH none)
+                    res=$(exec ${GPUPROG} $approach ${N} ${N} -1.5 0.5 -1 1 ${CA_MAXDWELL} ${g} ${r} ${B} $MAX_DEPTH none)
                     echo "done: ${res}"
                     a="${a},        ${res}"
                 done
                 echo $a >> ${OUTPUT}
-                echo $a
                 echo -e "\n"
             done
         done
