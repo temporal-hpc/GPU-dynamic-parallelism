4aa8a664e955a24d65929b13cce7fb3cd1e53937
Cristobal Navarro <cristobal.navarro.g@gmail.com>
2021-11-11 17:32:44 -0300

    fixed message order on ./prog

--- src/tools.cuh
+++ src/tools.cuh
@@ -18,9 +18,9 @@ void check_args_info(int argc){
         cout << "   rmin rmax ----------------- [-1.5, 0.5]     Real part range." << endl;
         cout << "   cmin cmax ----------------- [-1.0, 1.0]     Complex part range." << endl;
         cout << "   CA_MAXDWELL --------------- 512             Maximum numbers of iterarions per element." << endl;
-        cout << "   g ------------------------- 32               Initial numbers of regions (powers of 2)." << endl;
-        cout << "   B ------------------------- 32              Region Size for stopping subdivision (powers of 2)." << endl;
+        cout << "   g ------------------------- 32              Initial numbers of regions (powers of 2)." << endl;
         cout << "   r ------------------------- 4               Subdivision scheme (powers of 2)." << endl;
+        cout << "   B ------------------------- 32              Region Size for stopping subdivision (powers of 2)." << endl;
         cout << "   MAX_DEPTH ----------------- 5               Maximum recursion depth." << endl;
         cout << "   filename  ----------------- none            Chosen filename, 'none' to skip file output." << endl;
         exit(-1);
