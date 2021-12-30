f0a7d937d09289467d311ec2d6dc419da3d818b5
Cristobal Navarro <cristobal.navarro.g@gmail.com>
2021-12-30 16:59:23 -0300

    better benchmark prints

--- benchmark.sh
+++ benchmark.sh
@@ -12,15 +12,16 @@ CA_MAXDWELL=512
 MAX_DEPTH=1000
 DATE=$(exec date +"%T-%m-%d-%Y (%:z %Z)")
 echo "DATE = ${DATE}"
-OUTPUT=data/${STRING}-ARCH${ARCH}-REA${REAL}-REP${REPE}-BS${BS}.dat
+OUTPUT=data/${STRING}-ARCH${ARCH}-BS${BS}.dat
 
 # COMPILE
 #make -B ARCH=${ARCH} REALIZATIONS=${REAL}  REPEATS=${REPE} BSX=${BS} BSY=${BS} BENCHMARK=BENCHMARK
 echo "#NEW BENCHMARK ${STRING} ${ARCH}  ${DATE}        MAXDWELL=${CA_MAXDWELL}  MAX_DEPTH=${MAX_DEPTH}">> ${OUTPUT}
-echo "#N, g,r,B,                   perfAP0                             perfA1                            perfA2                               perfA3" >> ${OUTPUT}
+echo "#N, g,r,B, REAL,REP       perf-Exhaustive                             perf-DP-SBR                            perf-DP-MBR                           perf-ASK-SBR                         perf-ASK-MBR" >> ${OUTPUT}
 
 maxEXP=10
 
+AP=("Exhaustive" "DP-SBR" "DP-MBR" "ASK-SBR" "ASK-MBR")
 # REALIZATIONS ARRAY
 REAL=(16 16 16 16 16 16 16 16 16 8 8 4 4 4 4 4 4)
 # REPEATS
@@ -32,9 +33,10 @@ NexpMAX=16
 # RUN
 for ((size=0; size <= ${NexpMAX}; size++));
 do
-    make -B ARCH=${ARCH} REALIZATIONS=${REAL[${size}]}  REPEATS=${REPE} BSX=${BS} BSY=${BS} BENCHMARK=BENCHMARK
     N=$((2**${size}))
     lim=$((${size}<${maxEXP} ? ${size} : ${maxEXP}))
+    echo "Starting N=${N}"
+    make -B ARCH=${ARCH} REALIZATIONS=${REAL[${size}]}  REPEATS=${REPE} BSX=${BS} BSY=${BS} BENCHMARK=BENCHMARK
     for ((gexp=1; gexp <= ${lim}; gexp++));
     do
         g=$((2**${gexp}))
@@ -46,11 +48,11 @@ do
             for ((Bexp=0; Bexp <= ${divlimit}; Bexp++));
             do
                 B=$((2**${Bexp}))
-                a="${N},    ${g},${r},${B}"
+                a="${N},    ${g},${r},${B},    ${REAL[${size}]},${REPE}"
                 echo "BENCHMARK g=${g},r=${r},B=${B},    N=${N},  REALIZATIONS=${REAL[${size}]}  REPEATS=${REPE}"
                 for approach in 0 1 2 3 4
                 do
-                    echo -n -e "\tA${approach}......"
+                    printf '%10s......' ${AP[${approach}]}
                     res=$(exec ${GPUPROG} $approach ${N} ${N} -1.5 0.5 -1 1 ${CA_MAXDWELL} ${g} ${r} ${B} $MAX_DEPTH none)
                     echo "done: ${res}"
                     a="${a},        ${res}"
@@ -60,4 +62,6 @@ do
             done
         done
     done
+    echo "N=${N} Finished"
+    echo ""
 done
