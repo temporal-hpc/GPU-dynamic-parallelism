f537c83c6e2fd5cc7bdae3b92ed197e1c41d1705
Cristobal Navarro <cristobal.navarro.g@gmail.com>
2021-12-30 10:50:55 -0300

    updated benchmark to use different realizations as N grows

--- benchmark.sh
+++ benchmark.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-if [ "$#" -ne 5 ]; then
-    echo "run as ./benchmark.sh <STRING> <ARCH> <REALIZATIONS> <REPEATS> <BS>"
-    echo "Example: ./benchmark.sh A100 sm_80 4 8 32"
+if [ "$#" -ne 3 ]; then
+    echo "run as ./benchmark.sh <STRING> <ARCH> <BS>"
+    echo "Example: ./benchmark.sh A100 sm_80 32"
     exit
 fi
 STRING=$1
 ARCH=$2
-REAL=$3
-REPE=$4
-BS=$5
+BS=$3
 echo "REALIZATIONS=${REAL}  REPEATS=${REPE}"
 GPUPROG=./bin/gpuDP
 CA_MAXDWELL=512
@@ -18,16 +16,23 @@ echo "DATE = ${DATE}"
 OUTPUT=data/${STRING}-ARCH${ARCH}-REA${REAL}-REP${REPE}-BS${BS}.dat
 
 # COMPILE
-make -B ARCH=${ARCH} REALIZATIONS=${REAL}  REPEATS=${REPE} BSX=${BS} BSY=${BS} BENCHMARK=BENCHMARK
+#make -B ARCH=${ARCH} REALIZATIONS=${REAL}  REPEATS=${REPE} BSX=${BS} BSY=${BS} BENCHMARK=BENCHMARK
 echo "#NEW BENCHMARK ${STRING} ${ARCH}  ${DATE}        MAXDWELL=${CA_MAXDWELL}  MAX_DEPTH=${MAX_DEPTH}">> ${OUTPUT}
 echo "#N, g,r,B,                   perfAP0                             perfA1                            perfA2                               perfA3" >> ${OUTPUT}
 
 maxEXP=10
+
+# REALIZATIONS ARRAY
+REAL=(16 16 16 16 16 16 16 16 16 8 8 4 4 4 4 4 4)
+# REPEATS
+REPE=4
+
 NexpMAX=16
 
 # RUN
 for ((size=0; size <= ${NexpMAX}; size++));
 do
+    make -B ARCH=${ARCH} REALIZATIONS=${REAL[${size}]}  REPEATS=${REPE} BSX=${BS} BSY=${BS} BENCHMARK=BENCHMARK
     N=$((2**${size}))
     lim=$((${size}<${maxEXP} ? ${size} : ${maxEXP}))
     for ((gexp=1; gexp <= ${lim}; gexp++));
@@ -42,7 +47,7 @@ do
             do
                 B=$((2**${Bexp}))
                 a="${N},    ${g},${r},${B}"
-                echo "BENCHMARK g=${g},r=${r},B=${B},    N=${N}"
+                echo "BENCHMARK g=${g},r=${r},B=${B},    N=${N},  REALIZATIONS=${REAL[${size}]}  REPEATS=${REPE}"
                 for approach in 0 1 2 3 4
                 do
                     echo -n -e "\tA${approach}......"
